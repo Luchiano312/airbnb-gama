c0fd87333864ec7aa1d357241aaace123008586b
luciano <luchiano312@gmail.com>
2020-05-06 21:27:20 -0300

    melhorias do css

--- js/vanilla.js
+++ js/vanilla.js
@@ -18,6 +18,9 @@ request.onload = function(){
             const galeria = document.createElement('div')
             galeria.setAttribute('class','galeria col-xs-12 col-md-4')
 
+            const suporte = document.createElement('div')
+            suporte.setAttribute('class','suportediv')
+
             const photos = document.createElement('img');
             photos.setAttribute('class','photos-api ')
             photos.src = airBnb.photo;
@@ -30,14 +33,16 @@ request.onload = function(){
             h4.textContent = airBnb.name;
             h4.setAttribute('class','tamanho-h4')
 
-            const span = document.createElement('span')
-            span.textContent = airBnb.price;    
+            const h5 = document.createElement('h5')
+            h5.textContent = airBnb.price;
+            h5.setAttribute('class','tamanho-h5')    
 
             conteudo.appendChild(galeria);
-            galeria.appendChild(photos);
-            galeria.appendChild(h3);
-            galeria.appendChild(h4);
-            galeria.appendChild(span);
+            galeria.appendChild(suporte);
+            suporte.appendChild(photos);
+            suporte.appendChild(h3);
+            suporte.appendChild(h4);
+            suporte.appendChild(h5);
         })
     }else{
         console.log('Nao deu certo')
